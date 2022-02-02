6e1c3ce7e770f4e30afbc60e869b8f3ea8581559
Lenalimbo <iiidina@yahoo.com>
2022-02-02 19:30:38 +0300

    20220202

---
+++ resume/js/data.js
@@ -0,0 +1,157 @@
+HomeText([
+    {
+        "title":"Arian Ebrahimi",
+        "name":"Arian Ebrahimi",
+        "profilePic":"img/pp.jpg",
+        "email":"itsarianebrahimi@gmail.com",
+        "phone":"+989102625250",
+        "object":" I am looking for a challenging job with a rapidly growing organization that can provide me with a range of goals and job objectives within a contemporary and economical business setting. Mostly like to work in open-Source and GNU project. "
+    }
+]);
+Links([
+    {
+        "href":"https://github.com/ribrea",
+        "icon":"fab fa-github",
+    },
+    {
+        "href":"https://www.linkedin.com/in/arian-ebrahimi-7a85781bb/",
+        "icon":"fab fa-linkedin",
+    },
+    {
+        "href":"pdf/cv.pdf",
+        "icon":"fa fa-file-pdf",
+    },
+
+]);
+Skill([
+	{
+		"title":"Back-end Programming",
+	},
+	{
+		"title":"GNU/Linux",
+	},
+	{
+		"title":"Git"
+	},
+	{
+		"title":"DevOps"
+	},
+    {
+        "title" : "Rest and Restful api"
+    }
+]);
+Language([
+    {
+        "title":"Persian",
+
+    },
+    {
+        "title":"English",
+
+    },
+
+]);
+Framework([
+    {
+        "title":"Django",
+        "language" :"Python"
+    },
+    {
+        "title":"Vue",
+        "language" :"JavaScript"
+    },
+]);
+Experience([
+    {
+        "title":"Back-end Developer",
+        "at":"Stacrypt",
+        "des": "At Stacrypt we are shaping digital money and revolutionizing the way people transact monetarily by reaching out to consumers new to cryptocurrency in a fun and novel way. We attract people who constantly push themselves to think differently and chart exciting new paths in a rapidly growing industry. Currently, our main focus is on our Cryptocurrency Exchange and Trading Platform, meanwhile, we are working on various great ideas and innovative software.",
+        "startDate":2021,
+        "endDate":2022,
+    },
+
+    
+    {
+        "title":"Back-end Developer",
+        "at":"Supnex",
+        "des": "Our goal is to provide simple, beautiful, and effective software solutions your team will enjoy using. From performance and uptime monitoring to large scale log management, we have a variety of solutions to help make your products the best they can be.",
+        "startDate":2020,
+        "endDate":2021,
+    },
+{
+        "title":"Lead Web Developer",
+        "at":"Aptus Iran",
+        "des":"Aptus Iran Company is established on 1981, and with three decades of experience in construction industry, is known as one of the major suppliers of Pavers,Blocks,Joists and Ready Mixed Concert. Aptus Iran is also known as a General Contractor certified with the highest ranking in Mass Construction and Water industry.",
+        "startDate":2019,
+        "endDate":2020,
+    },
+    {
+        "title":"System Administrator",
+        "at":"Aptus Iran",
+        "des":"Aptus Iran Company is established on 1981, and with three decades of experience in construction industry, is known as one of the major suppliers of Pavers,Blocks,Joists and Ready Mixed Concert. Aptus Iran is also known as a General Contractor certified with the highest ranking in Mass Construction and Water industry.",
+        "startDate":2018,
+        "endDate":2019,
+    },
+    {
+        "title":"Back-end Developer",
+        "at":"Self Employed",
+        "des" :"Essentially, a freelance job is one where a person works for themselves, rather than for a company. While freelancers do take on contract work for companies and organizations, they are ultimately self-employed. ... Freelancers are not considered “employees” by the companies they work for, but rather “contractors.”",
+        "startDate":2016,
+        "endDate":2021,
+    },
+
+
+]);
+Education([
+    {
+        "title":"University of Zanjan",
+        "study":"Computer Engineering",
+        "grade":"Bachelor's degree",
+        "endDate":2024,
+    },
+
+
+]);
+Project([
+    {
+        "title":"Contributing on Django Core",
+        "link":"https://github.com/ribrea/django",
+        "href":"https://github.com/ribrea/django",
+        "des":"Django is a Python-based free and open-source web framework that follows the model–template–views architectural pattern. It is maintained by the Django Software Foundation, an American independent organization established as a 501 non-profit."
+    },
+    {
+        "title":"Contributing on Django Rest Framework",
+        "link":"https://github.com/ribrea/django-rest-framework",
+        "href":"https://github.com/ribrea/django-rest-framework",
+        "des":"Web APIs for Django",
+    },
+    {
+        "title":"Blogo",
+        "link":"https://github.com/ribrea/blogo",
+        "href":"https://github.com/ribrea/blogo",
+        "des":"A way to crate blog ",
+    },
+
+]);
+Activity([
+    {
+        "title":"Stack overflow",
+        "des":"Stack Overflow is a question and answer site for professional and enthusiast programmers. It is a privately held website, the flagship site of the Stack Exchange Network, created in 2008 by Jeff Atwood and Joel Spolsky. It features questions and answers on a wide range of topics in computer programming",
+    },
+    {
+        "title":"GNU community",
+        "des":"GNU is an extensive collection of free software, which can be used as an operating system or can be used in parts with other operating systems. The use of the completed GNU tools led to the family of operating systems popularly known as Linux. Most of GNU is licensed under the GNU Project's own General Public License."
+    },
+
+]);
+Article([
+
+]);
+
+
+
+
+
+
+
+
